9015a587be6c7f6980126dbf82b9b8e8a3310921
Sindhuja1210 <sindhuja121020@gmail.com>
2024-09-30 21:10:58 +0530

    updated hello world

--- Practice/src/helloworld/Hello.java
+++ Practice/src/helloworld/Hello.java
@@ -3,6 +3,8 @@ package helloworld;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello world!!");
+		for(int i=0;i<=2;i++)
+			System.out.println("hai babe");
 	}
 
 }
